bf4850ecae2b7c2b21e8e163e137eb209c7d20b6
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-09-26 23:58:20 -0400

    Initial commit.

---
+++ reviewvectors.cpp
@@ -0,0 +1,33 @@
+//
+// Created by mr47 on 9/26/17.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    vector<string> myString(5);
+    myString[0] = "one";
+    myString[1] = "two";
+    myString[2] = "three";
+    myString[3] = "four";
+    myString[4] = "five";
+
+
+    for(int i=0; i<myString.size(); i++){
+        cout<<myString[i] <<endl;
+    }
+
+    cout << endl << endl;
+
+    myString.pop_back();
+
+    for(int i=0; i<myString.size(); i++){
+        cout<<myString[i] <<endl;
+    }
+
+    return 0;
+}
