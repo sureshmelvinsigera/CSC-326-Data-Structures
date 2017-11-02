88bb506e5435c168e58425d78399715ce2d6fe6f
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-11-02 09:44:02 -0400

    Initial commit.

---
+++ adtbag.cpp
@@ -0,0 +1,20 @@
+//
+// Created by mr47 on 11/1/17.
+//
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class ATDBag{
+public:
+    ATDBag();
+public:
+
+};
+
+int main() {
+    string bagContent[] = {"One", "Two", "Three", "Four"};
+
+}
